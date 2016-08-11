5f052c0c7421374ad8d666d30f3f408c9f8e31a8
zhaocunming <1017355712@qq.com>
2016-08-11 18:20:07 +0800

    NewProjects

---
+++ app/src/main/java/com/zcm/practice/thunder/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.zcm.practice.thunder.Activity;
+
+import com.zcm.practice.thunder.BaseActivity;
+
+/**
+ * Copyright(c) 2016 All Rights Reserved.
+ * Author: ZhaoCunming
+ * Time: 16-8-11 16:29
+ * E-mail: zhaocunming@dayima.com
+ * Version：V1.0.1
+ * ProjectName: Thunder
+ * Description: TODO
+ */
+public class MainActivity extends BaseActivity {
+}
---
+++ app/src/main/java/com/zcm/practice/thunder/BaseActivity.java
@@ -0,0 +1,21 @@
+package com.zcm.practice.thunder;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Copyright(c) 2016 All Rights Reserved.
+ * Author: ZhaoCunming
+ * Time: 16-8-11 16:10
+ * E-mail: zhaocunming@dayima.com
+ * Version：V1.0.1
+ * ProjectName: Thunder
+ * Description: TODO
+ */
+public class BaseActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
