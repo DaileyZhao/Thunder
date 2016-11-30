8809c38dd0e175b27af4cdfd19933db71696ccdf
zhaocunming <1017355712@qq.com>
2016-11-30 19:09:31 +0800

    okhttp

---
+++ thunder/src/main/java/com/android/thunder/http/ApiServers.java
@@ -0,0 +1,13 @@
+package com.android.thunder.http;
+
+/**
+ * Copyright(c) 2016 All Rights Reserved.
+ * Author: ZhaoCunming
+ * Time: 16-11-30 16:32
+ * E-mail: zhaocunming@dayima.com
+ * Version：V1.0.1
+ * ProjectName: Thunder
+ * Description: TODO
+ */
+public interface ApiServers {
+}
